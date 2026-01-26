6ddb6140525e8efff7f01d4808ad3c62191ce2e1
Luke Sapp <53846468+Mr-Freez@users.noreply.github.com>
2026-01-26 15:16:54 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// API endpoint for ESP32 updates
+let latestData = null;
+app.post('/api/update', function(req, res) {
+  latestData = req.body;
+  req.app.set('latestData', latestData);
+  console.log('ESP32 data:', latestData);
+
+  const io = req.app.get('io');
+  if (io) io.emit('sensor-update', latestData);
+
+  res.json({ status: 'ok' });
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.use(express.json());
+
+let latestData = null;
+
+app.get("/", (req, res) => {
+  res.send("Website is running");
+});
+
+
+// ESP32 sends data here
+app.post("/api/update", (req, res) => {
+  latestData = req.body;
+  console.log("ESP32 data:", latestData);
+
+  io.emit("sensor-update", latestData);
+  res.json({ status: "ok" });
+});
+
+// Website connects here
+io.on("connection", (socket) => {
+  console.log("Website connected");
+
+  if (latestData) {
+    socket.emit("sensor-update", latestData);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
